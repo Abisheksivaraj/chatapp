12e96fc9f172fe1b3723e8b94db86be02fee0ac6
Abisheksivaraj <abisheksivaraj2001@gmail.com>
2023-11-23 23:31:37 +0530

    sdfgh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <div className="navbar">
+      <div className="logo-nav">Chat Hub...!</div>
+
+      <div className="client-img">
+        <img src="" alt="" />
+      </div>
+      <span>Abishek</span>
+      <button className="logout">
+        logout
+        <img
+          src="https://t4.ftcdn.net/jpg/00/77/21/45/360_F_77214592_nwi23yr4rsRsuA0yy6FspW7AiRFTq5NU.jpg"
+          alt=""
+        />
+      </button>
+    </div>
+  );
 }
 
 export default Navbar
